557c7495182576eb6accc9ecdbdf74551cac0ced
Steven Snyder <sts12589@gmail.com>
2020-07-13 10:27:37 -0400

    Part Two completed

--- solution_queries.sql
+++ solution_queries.sql
@@ -8,13 +8,19 @@ LEFT JOIN EMPLOYEES ON CUSTOMERS.salesRepEmployeeNumber=employees.employeeNumber
 ORDER BY customers.customerName ASC;
 
 
-
-
 -- Part 2
 -- Determine which products are most popular with our customers.
 -- For each product, list the total quantity ordered along with the total sale generated (total quantity ordered * priceEach) for that product.
 -- The column headers should be Product Name, Total # Ordered and Total Sale.
 -- List the products by Total Sale descending.
+
+SELECT * FROM products;
+SELECT * FROM payments;
+SELECT * FROM orderdetails;
+SELECT productName AS 'Product Name',(quantityOrdered * priceEach) AS 'Total sale' 
+FROM products JOIN orderdetails ON products.productcode=orderdetails.productcode 
+ORDER BY  (quantityOrdered * priceEach) desc;
+
 -- Part 3
 -- Write a query which lists order status and the # of orders with that status.
 -- Column headers should be Order Status and # Orders.
